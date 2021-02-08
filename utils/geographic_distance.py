5d9cd165bb61d78d7b4c0494e12568466c04ac2c
Clement de Loubresse <c.garreaudl@gmail.com>
2021-02-08 16:44:29 +0100

    Updated distance function

--- utils/geographic_distance.py
+++ utils/geographic_distance.py
@@ -1,7 +1,6 @@
-import math
+import numpy as np
 
 def geographic_distance(lat_runway,long_runway,lat_stand,long_stand):
-    
     '''
     Computes distance between two sets of coordinates:
     - (lat_runway, long_runway): geographical coordinates of RUNWAY
@@ -10,19 +9,19 @@ def geographic_distance(lat_runway,long_runway,lat_stand,long_stand):
 
     EARTH_RADIUS = 6371
 
-    lat_runway_rad = math.radians(lat_runway)
-    long_runway_rad = math.radians(long_runway)
-    lat_stand_rad = math.radians(lat_stand)
-    long_stand_rad = math.radians(long_stand)
+    lat_runway_rad = np.radians(lat_runway)
+    long_runway_rad = np.radians(long_runway)
+    lat_stand_rad = np.radians(lat_stand)
+    long_stand_rad = np.radians(long_stand)
 
     dist_long = long_stand_rad - long_runway_rad
     dist_lat =  lat_stand_rad - lat_runway_rad
 
-    a = sin(dist_lat / 2)**2 +cos(lat_stand_rad) * cos(lat_runway_rad)
-    a *= sin(dist_long / 2)**2
+    a = np.sin(dist_lat / 2)**2 +np.cos(lat_stand_rad) * np.cos(lat_runway_rad)
+    a*= np.sin(dist_long / 2)**2
 
-    c = 2 * math.atan2(math.sqrt(a),math.sqrt(1-a))
+    c = 2 * np.arctan2(np.sqrt(a),np.sqrt(1-a))
 
-    distance = EARTH_RADIUS*c
+    distance = EARTH_RADIUS * c
 
     return distance
