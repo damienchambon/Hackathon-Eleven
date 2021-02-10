7280bf898990b04eecdd859e5d07db7d7c8be06d
Damien Chambon <damien_chambon@outlook.com>
2021-02-10 15:56:34 +0100

    Update join_datasets.py

--- utils/join_datasets.py
+++ utils/join_datasets.py
@@ -46,7 +46,8 @@ def join_datasets(df_airport, df_weather=None,
         # getting the coordinates of the stands
         df_geography_stand = df_geography[['stand', 'Lat_stand', 'Lng_stand']]
         df_geography_stand_no_dup = df_geography_stand.drop_duplicates()
-        df_geography_stand_no_dup['stand'] = df_geography_stand_no_dup['stand'].str.upper()
+        df_geography_stand_no_dup['stand'] = \
+            df_geography_stand_no_dup['stand'].str.upper()
 
         # merging merged_df with df_geography
         merged_df = merged_df.merge(df_geography_runway_no_dup,
@@ -64,13 +65,16 @@ def join_datasets(df_airport, df_weather=None,
         pairs_rename_aircraft_models = {}
         pairs_rename_aircraft_models['A320-100/200'] = 'A320-200'
         pairs_rename_aircraft_models['A319'] = 'A319-100'
-        pairs_rename_aircraft_models['B737-800 WINGLETS'] = '737-800 with winglets'
+        pairs_rename_aircraft_models['B737-800 WINGLETS'] = \
+            '737-800 with winglets'
         pairs_rename_aircraft_models['A321-100/200'] = 'A321-100'
         pairs_rename_aircraft_models['B777-200'] = '777-200'
         pairs_rename_aircraft_models['B787-900'] = '787-9 Dreamliner'
         pairs_rename_aircraft_models['ERJ-195'] = 'EMB 195 Standard'
-        pairs_rename_aircraft_models['B757-200 WINGLETS'] = '757-200, -200PF with winglets'
-        pairs_rename_aircraft_models['B787-800 Dreamliner'] = '787-8 Dreamliner'
+        pairs_rename_aircraft_models['B757-200 WINGLETS'] = \
+            '757-200, -200PF with winglets'
+        pairs_rename_aircraft_models['B787-800 Dreamliner'] = \
+            '787-8 Dreamliner'
         pairs_rename_aircraft_models['B747-400 Passenger'] = '747-400F'
         pairs_rename_aircraft_models['A320 NEO'] = 'A320neo Sharklet'
         pairs_rename_aircraft_models['A330-200'] = 'A330-200'
@@ -79,7 +83,8 @@ def join_datasets(df_airport, df_weather=None,
         pairs_rename_aircraft_models['ATR 72-500'] = 'ATR-42-500/"600"'
         pairs_rename_aircraft_models['B737-400'] = '737-400'
         pairs_rename_aircraft_models['ERJ-190'] = 'EMB 190 Standard'
-        pairs_rename_aircraft_models['B737-700 WINGLETS'] = '737-700 with winglets'
+        pairs_rename_aircraft_models['B737-700 WINGLETS'] = \
+            '737-700 with winglets'
         pairs_rename_aircraft_models['A350-900'] = 'A350-900'
         pairs_rename_aircraft_models['DASH 8-Q400'] = 'Dash 8 Q400'
         pairs_rename_aircraft_models['B737-900ER'] = '737-900ER'
